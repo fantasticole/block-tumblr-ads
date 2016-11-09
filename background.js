0e197017fb111d8f0f395d4cc3444fa8432596f0
Cole Murphy <cole.murphy@venuebook.com>
2016-11-09 11:58:33 -0500

    Remove toggling functionality.

--- background.js
+++ background.js
@@ -1,24 +1,3 @@
-function toggleSchim () {
-	if (getStorage('displaySchim') === 'true') {
-		chrome.tabs.executeScript(null, {file: "hideSchim.js"});
-		setStorage('displaySchim', 'false')
-	}
-	else {
-		chrome.tabs.executeScript(null, {file: "showSchim.js"});
-		setStorage('displaySchim', 'true')
-	}
-}
-
-function setStorage(key, val) {
-  localStorage.setItem(key, JSON.stringify(val));
-};
-
-function getStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
-};
-
-chrome.browserAction.onClicked.addListener(toggleSchim);
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     // read `newIconPath` from request and read `tab.id` from sender
     chrome.browserAction.setIcon({
--- hideSchim.js
+++ hideSchim.js
@@ -2,54 +2,32 @@ function hideSchim () {
 	var tagList = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ul', 'li', 'span', 'em'],
 		allTagsFromList = document.querySelectorAll(tagList),
 		title = document.getElementsByTagName('title')[0],
-		schimmasks = document.getElementsByClassName('schimmask'),
 		newTitle;
 
-	// if words have been changed
-	if (schimmasks.length > 0) {
-		// change each one back
-		for (i = 0; i < schimmasks.length; i++) {
-			var oldString = schimmasks[i].innerHTML,
-				newString = oldString
-					.replace(/Schimanski/g, "Verboten")
-					.replace(/schimanski/g, "verboten");
-			// update the current element's text
-			schimmasks[i].innerHTML = newString;
-		}
-		// if the page title has been changed
-		if (localStorage[title.innerHTML]) {
-			// update the page title
-			document.getElementsByTagName('title')[0].innerHTML = localStorage[title.innerHTML];
-		}
-	} else {
-		// set the text inside of the document
-		for (i = 0; i < allTagsFromList.length; i++) {
-			var oldString = document.querySelectorAll(tagList)[i].innerHTML,
-				newString = oldString
-					.replace(/Schimanski/g, "<span class='schimmask'>Verboten</span>")
-					.replace(/schimanski/g, "<span class='schimmask'>verboten</span>");
+	// set the text inside of the document
+	for (i = 0; i < allTagsFromList.length; i++) {
+		var oldString = document.querySelectorAll(tagList)[i].innerHTML,
+			newString = oldString
+				.replace(/Schimanski/g, "<span class='schimmask'>Verboten</span>")
+				.replace(/schimanski/g, "<span class='schimmask'>verboten</span>");
 
-			// update the current element's text
-			document.querySelectorAll(tagList)[i].innerHTML = newString;
-		}
+		// update the current element's text
+		document.querySelectorAll(tagList)[i].innerHTML = newString;
+	}
 
-		// set the text for the page title
-		newTitle = title.innerHTML
-			.replace(/Schimanski/g, "Verboten")
-			.replace(/schimanski/g, "verboten");
+	// set the text for the page title
+	newTitle = title.innerHTML
+		.replace(/Schimanski/g, "Verboten")
+		.replace(/schimanski/g, "verboten");
 
-		// if the title changes
-		if (newTitle !== title.innerHTML) {
-			// set the new and old titles on local storage
-			localStorage[newTitle] = title.innerHTML;
-			localStorage[title.innerHTML] = newTitle;
-			// and update the page title
-			document.getElementsByTagName('title')[0].innerHTML = newTitle;
-		}
+	// if the title changes
+	if (newTitle !== title.innerHTML) {
+		// set the new and old titles on local storage
+		localStorage[newTitle] = title.innerHTML;
+		localStorage[title.innerHTML] = newTitle;
+		// and update the page title
+		document.getElementsByTagName('title')[0].innerHTML = newTitle;
 	}
 }
 
 hideSchim();
-
-// update icon to show status
-chrome.runtime.sendMessage({ "newIconPath" : 'v_icon2.png' });
